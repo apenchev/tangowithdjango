2ce7989a973a7de9a795330566fa1b7f4471dce9
Atanas Penchev <atanas.penchev@outlook.com>
2015-01-18 23:35:54 +0000

    Chapter 4 - Completed

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("""Rango says hey there world!
+    	<a href=\"./about\">About</a>""")
+
+def about(request):
+	return HttpResponse("""Rango says here is the about page.
+		Atanas Penchev, 2072742
+		<a href=\"./..\">Home</a>""")
