52039ddc2ae265e1823dae237d3a53389c8d7294
Atanas Penchev <atanas.penchev@outlook.com>
2015-02-08 00:44:29 +0000

    Chapter 9 - Registered the new User model.

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 class PageAdmin(admin.ModelAdmin):
-	list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+
+admin.site.register(UserProfile)
